dde0d46dbf3e26d6134571579046778ad0011175
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-08-30 13:48:02 +0300

    End of Lesson 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.Lesson1.GameWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        // Lesson 1
+        new GameWindow();
+    }
+}
