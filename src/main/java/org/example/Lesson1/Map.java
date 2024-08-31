f543b6e95c1dc34f49a59616fea67f0086987736
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-08-31 11:15:46 +0300

    Conditions has been deleted from checkWin and I added cycles

--- src/main/java/org/example/Lesson1/Map.java
+++ src/main/java/org/example/Lesson1/Map.java
@@ -42,7 +42,7 @@ public class Map extends JPanel {
         isInitialized = false;
     }
 
-    public void startNewGame(int mode, int fSzX, int fSzY, int wLen) {
+    public void startNewGame(int mode, int fSzX, int fSzY, int wLen) { // Запуск новой игры
         System.out.printf("Mode: %d;\nSize: x=%d, y=%d;\nWin Length: %d,",
                 mode, fSzX, fSzY, wLen);
         initMap();
@@ -64,7 +64,7 @@ public class Map extends JPanel {
         if (checkEndGame(AI_DOT, STATE_WIN_AI)) return;
     }
 
-    private boolean checkEndGame(int dot, int gameOverType) {
+    private boolean checkEndGame(int dot, int gameOverType) { // Проверка на окончание игры
         if (checkWin(dot)) {
             this.gameOverType = gameOverType;
             isGameOver = true;
@@ -72,7 +72,7 @@ public class Map extends JPanel {
             return true;
         }
         if (isMapFull()) {
-            this.gameOverType = gameOverType;
+            this.gameOverType = STATE_DRAW;
             isGameOver = true;
             repaint();
             return true;
@@ -86,7 +86,7 @@ public class Map extends JPanel {
         render(g);
     }
 
-    private void initMap() {
+    private void initMap() { // Инициализация карты
         fieldSizeY = 3;
         fieldSizeX = 3;
         field = new char[fieldSizeY][fieldSizeX];
@@ -97,15 +97,15 @@ public class Map extends JPanel {
         }
     }
 
-    private boolean isValidCell(int x, int y) {
+    private boolean isValidCell(int x, int y) { // Проверка диапозонов
         return x >= 0 && x < fieldSizeX && y >= 0 && y < fieldSizeY;
     }
 
-    private boolean isEmptyCell(int x, int y) {
+    private boolean isEmptyCell(int x, int y) { // Проверка на пустоту ячейки
         return field[y][x] == EMPTY_DOT;
     }
 
-    private void aiTurn() {
+    private void aiTurn() { // Ход искусственного интелекта
         int x, y;
         do {
             x = RANDOM.nextInt(fieldSizeX);
@@ -114,7 +114,7 @@ public class Map extends JPanel {
         field[y][x] = AI_DOT;
     }
 
-    private boolean isMapFull() {
+    private boolean isMapFull() { // Проверка на заполненость карты
         for (int i = 0; i < fieldSizeY; i++) {
             for (int j = 0; j < fieldSizeX; j++) {
                 if (field[i][j] == EMPTY_DOT) return false;
@@ -186,16 +186,41 @@ public class Map extends JPanel {
     }
 
     private boolean checkWin(int c) { // Проверка на победу одной из сторон
-        if (field[0][0]==c && field[0][1]==c && field[0][2]==c) return true;
-        if (field[1][0]==c && field[1][1]==c && field[1][2]==c) return true;
-        if (field[2][0]==c && field[2][1]==c && field[2][2]==c) return true;
-
-        if (field[0][0]==c && field[1][0]==c && field[2][0]==c) return true;
-        if (field[0][1]==c && field[1][1]==c && field[2][1]==c) return true;
-        if (field[0][2]==c && field[1][2]==c && field[2][2]==c) return true;
-
-        if (field[0][0]==c && field[1][1]==c && field[2][2]==c) return true;
-        if (field[0][2]==c && field[1][1]==c && field[2][0]==c) return true;
+        int counterX = 0;
+        int counterY = 0;
+        int counterDiagonal1 = 0;
+        int counterDiagonal2 = 0;
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                char firstElement1 = field[i][0];
+                char firstElement2 = field[0][i];
+                char firstElement3 = field[0][0];
+                char firstElement4 = field[0][fieldSizeX-1];
+                if (field[i][j] == firstElement1 && firstElement1 != EMPTY_DOT) {
+                    counterX++;
+                }
+                if (field[j][i] == firstElement2 && firstElement2 != EMPTY_DOT) {
+                    counterY++;
+                }
+                if (field[j][j] == firstElement3 && firstElement3 != EMPTY_DOT) {
+                    counterDiagonal1++;
+                }
+                if (field[i][j] == firstElement4 && i + j == 2 && firstElement4 != EMPTY_DOT) {
+                    counterDiagonal2++;
+                }
+            }
+            if (counterX == fieldSizeX)
+                return true;
+            else if (counterY == fieldSizeY)
+                return true;
+            else if (counterDiagonal1 == fieldSizeX)
+                return true;
+            counterX = 0;
+            counterY = 0;
+            counterDiagonal1 = 0;
+        }
+        if (counterDiagonal2 == fieldSizeX)
+            return true;
         return false;
     }
 
